abc74d28e518331eb5982ae4100f4b615680c315
Alex Lee <alex.lee@unity3d.com>
2023-01-12 02:20:21 -0500

    add invited param and donation accounts

--- wedding-invitation/scripts/main.js
+++ wedding-invitation/scripts/main.js
@@ -1,45 +1,27 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 $(document).ready(function () {
-    $("#map-image").on("click")
-    {
-
-    }
-
     $('#go-to-top').click(function () {
         $('html,body').animate({scrollTop: 0}, 400);
         return false;
     });
 
-    $(".gift-send").click(function () {
-        $("#gift-name").text($(this).data("name"));
-    })
-
-
-    $("#reserveGiftButton").click(function () {
-        let name = $("#sender-name").val();
-        let message = $("#sender-message").val();
-        $("#reserveGiftButton").text("전송중...");
-        $("#reserveGiftButton").prop("disabled", true);
-
-        emailjs.init("user_yjLL5xG0A3kkOCH5BGIDh");
-        emailjs.send("wedding-mail", "gift_send", {
-            name: name,
-            gift: $("#gift-name").text(),
-            message: message
-        }).then(function (response) {
-            $('#giftMailModal').modal('hide');
-            alert(name + "님의 메시지가 정상적으로 전송되었습니다.");
-
-            $("#reserveGiftButton").text("예약하기!");
-            $("#sender-name").val('');
-            $("#sender-message").val('');
-            $("#reserveGiftButton").prop("disabled", false);
-        }, function (err) {
-            alert("메시지 전송이 실패했습니다. 다시 시도해주세요.");
-        });
-    })
+    $('#go-to-top').click(function () {
+        $('html,body').animate({scrollTop: 0}, 400);
+        return false;
+    });
+    
+    hideInvitedElements()
 })
+
+function hideInvitedElements() {
+    const urlParams = new URL(location.href).searchParams;
+    const invited = urlParams.get('invited');
+    document.querySelectorAll("[invited]").forEach(function (item) {
+        if (invited == null)
+            item.style.display = 'none';
+    });
+  }
 
 // Smooth scroll for links with hashes
 $("a.smooth-scroll").click(function (event) {
