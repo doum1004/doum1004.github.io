b8e358341310ae619b2c43ba13a04d59e469e560
JEHYEOK LEE <doum1004@gmail.com>
2024-03-20 05:10:15 -0400

    test flutter web

--- flutter/flutter_service_worker.js
+++ flutter/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7f66a9d5d27cc9fdc7e6b85e9c80e750",
-"/": "7f66a9d5d27cc9fdc7e6b85e9c80e750",
-"main.dart.js": "d2fed71855b833957b1f27a743757785",
+"index.html": "e4c5f8be291a7f39059bdeb82795330b",
+"/": "e4c5f8be291a7f39059bdeb82795330b",
+"main.dart.js": "45741a9876bd0477b927a44e8db5b70f",
 "manifest.json": "7a9b7dbb78876d18c099c46828280892",
 "version.json": "bd0457fd6a44f420303f2e6f946d06a6"};
 // The application shell files that are downloaded before a service worker can
